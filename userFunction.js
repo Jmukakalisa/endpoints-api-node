627c59f3b8a5d250d39287fc731530f8fd07afab
Jmukakalisa <90443432+Jmukakalisa@users.noreply.github.com>
2023-02-10 17:57:25 +0200

    Delete userFunction.js

--- userFunction.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-import userModel from "./models/userModels";
-
-const secret = 'secret123';
-
-export function getUserFromToken(token) {
-  const userInfo = jwt.verify(token, secret);
-  return userModel.findById(userInfo.id);
-}
