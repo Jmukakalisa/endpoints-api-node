8d8c44c32078458aad251664e432f8ce85c1a9b9
Jmukakalisa <j.mukakalis@alustudent.co>
2023-02-17 15:36:21 +0200

    railway and new test commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
   });
 });
 app.listen(`${PORT}`, () => {
-  console.log(`Server has started on http://localhost:${PORT} `);
+  console.log(`Server has started on http://localhost:3000 `);
 });
 // if (process.env.NODE_ENV !== 'test') {
   mongoose.set('strictQuery', false);
